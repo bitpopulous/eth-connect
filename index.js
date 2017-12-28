11b49a8ba871b66603c1df83799e776d7eb4500e
Wisdom <wisop112@hotmail.com>
2017-12-28 19:51:29 +0000

    added populous contract functions

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ import {
 var settings = {
     web3: connect(network.ropsten)
 }
+
+
+// populous.transfer(settings.web3, contract.populous, '0xdb09c99a80254e6821640b8a5c21c7366cf8ff35', to, currency, amount).then(result => {
+//     console.log(result);
+// })
+
+populous.getLedgerEntry(settings.web3, contract.populous, '0xdb09c99a80254e6821640b8a5c21c7366cf8ff35', 'GBP', 'Populous').then(result => {
+    console.log(result);
+})
+
+// populous.mintTokens(settings.web3, contract.populous, '0xdb09c99a80254e6821640b8a5c21c7366cf8ff35', 'GBP', 400).then(result => {
+//     console.log(result);
+// })
