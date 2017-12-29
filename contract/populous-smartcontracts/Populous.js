90ab5fd1c5cd80461b133c7bb57c444c943eeb29
dibrovadev <oleksii.dibrova@gmail.com>
2017-12-29 12:40:15 +0200

    Remove unnecessary Promises from methods

--- contract/populous-smartcontracts/Populous.js
+++ contract/populous-smartcontracts/Populous.js
@@ -4,88 +4,46 @@ import {
 
 module.exports = {
 
-
   //Example, when dealing with constant methods or 'view' methods which only get data
   //returns crowdsale
   getCurrency: (web3, contract, from, currency) => {
-    return new Promise((resolve, reject) => {
-      const contractInstance = new web3.eth.Contract(contract.abi, contract.address);
+    const contractInstance = new web3.eth.Contract(contract.abi, contract.address);
 
-      contractInstance.methods.getCurrency(web3.utils.asciiToHex(currency)).call({
-        from: from
-      }, function (error, result) {
-        if (!error)
-          resolve(result)
-        else {
-          reject(new Error(error))
-        }
-
-      });
-
-    })
+    return contractInstance.methods
+      .getCurrency(web3.utils.asciiToHex(currency))
+      .call({from: from});
   },
 
   transfer: (web3, contract, from, to, currency, amount) => {
-    return new Promise((resolve, reject) => {
-      const contractInstance = new web3.eth.Contract(contract.abi, contract.address);
+    const contractInstance = new web3.eth.Contract(contract.abi, contract.address);
 
-      contractInstance.methods.transfer(
+    return contractInstance.methods
+      .transfer(
         web3.utils.asciiToHex(currency),
         web3.utils.asciiToHex(from),
         web3.utils.asciiToHex(to),
         amount
-      ).send({
-        from: from
-      })
-        .then(function (receipt) {
-          resolve(receipt);
-          // receipt can also be a new contract instance, when coming from a "contract.deploy({...}).send()"
-        })
-        .catch(Error => {
-          reject(new Error(Error));
-        })
-    })
-
+      )
+      .send({from: from});
   },
+
   getLedgerEntry: (web3, contract, from, currency, accountId) => {
-    return new Promise((resolve, reject) => {
-      const contractInstance = new web3.eth.Contract(contract.abi, contract.address);
+    const contractInstance = new web3.eth.Contract(contract.abi, contract.address);
 
-      contractInstance.methods.getLedgerEntry(
+    return contractInstance.methods
+      .getLedgerEntry(
         web3.utils.asciiToHex(currency),
-        web3.utils.asciiToHex(accountId)).call({
-        from: from
-      }, function (error, result) {
-        if (!error)
-          resolve(result)
-        else {
-          reject(new Error(error))
-        }
-
-      });
-
-    })
+        web3.utils.asciiToHex(accountId))
+      .call({from: from});
   },
+
   mintTokens: (web3, contract, from, currency, amount) => {
-    return new Promise((resolve, reject) => {
+    const contractInstance = new web3.eth.Contract(contract.abi, contract.address);
 
-      const contractInstance = new web3.eth.Contract(contract.abi, contract.address);
-
-
-      contractInstance.methods.mintTokens(
+    return contractInstance.methods
+      .mintTokens(
         web3.utils.asciiToHex(currency),
-        amount).send({
-        from: from
-      })
-        .then(function (receipt) {
-          resolve(receipt);
-          // receipt can also be a new contract instance, when coming from a "contract.deploy({...}).send()"
-        })
-        .catch(Error => {
-          reject(new Error(Error));
-        })
-
-    })
+        amount)
+      .send({from: from});
   }
-
-}
+};
