bde2afce5983bb1246f4fca63de27aba44eefe95
dibrovadev <oleksii.dibrova@gmail.com>
2018-01-15 11:50:31 +0200

    Dibrova dev (#40)
    * 1.0.7
    
    * Change account fail error catch method, add preversion hook and add test to precommit hook

--- src/methods/account.spec.js
+++ src/methods/account.spec.js
@@ -1,14 +1,14 @@
 import { expect } from 'chai';
 import account from './account';
-import {connect} from './connect';
+import { connect } from './connect';
 import { network } from '../config';
 
 var settings = {
-    web3: connect(network.ropsten)
+  web3: connect(network.ropsten)
 }
 describe('The unlock account method', () => {
   it('successfully unlocks the account', done => {
-    account.unlock(settings.web3,'0xbdc82ccac9fd5d90c5b8a87d1b3c22a88b3411bd','password9', 400)
+    account.unlock(settings.web3, '0xbdc82ccac9fd5d90c5b8a87d1b3c22a88b3411bd', 'password9', 400)
       .then(result => {
         expect(result).to.be.true;
         done();
@@ -18,17 +18,22 @@ describe('The unlock account method', () => {
   });
 
   it('returns an error when unlocking fails', done => {
-    account.unlock(settings.web3,'fail','password91', 400)
-      .catch(e => {
-        expect(e.message).to.be.a('string');
-        done();
-      });
+    try {
+      account.unlock(settings.web3, 'fail', 'password91', 400)
+        .catch(e => {
+          expect(e.message).to.be.a('string');
+          done();
+        });
+    } catch (error) {
+      expect(error.message).to.be.a('string');
+      done();
+    }
   });
 });
 
 describe('The lock account method', () => {
   it('successfully locks the account', done => {
-    account.lock(settings.web3,'0xbdc82ccac9fd5d90c5b8a87d1b3c22a88b3411bd','password9', 400)
+    account.lock(settings.web3, '0xbdc82ccac9fd5d90c5b8a87d1b3c22a88b3411bd', 'password9', 400)
       .then(result => {
         expect(result).to.be.true;
         done();
@@ -36,12 +41,17 @@ describe('The lock account method', () => {
       .catch(e => e)
       .finally(e => expect(e).to.be.undefined);
   });
-  
+
   it('returns an error when locking fails', done => {
-    account.lock(settings.web3,'fail','password9', 400)
-      .catch(e => {
-        expect(e.message).to.be.a('string');
-        done();
-      });
+    try {
+      account.lock(settings.web3, 'fail', 'password9', 400)
+        .catch(e => {
+          expect(e.message).to.be.a('string');
+          done();
+        });
+    } catch (error) {
+      expect(error.message).to.be.a('string');
+      done();
+    }
   });
 });
