b63789aba4b6528ebfdc58dfe0c49b3bf954686e
dibrovadev <oleksii.dibrova@gmail.com>
2018-01-17 13:39:08 +0200

    Placed smart contracts as static JS objects

---
+++ src/config/contractsConfigs/index.js
@@ -0,0 +1,13 @@
+import Populous from './Populous';
+import Crowdsale from './Crowdsale';
+import CurrencyToken from './CurrencyToken';
+import DepositContractsManager from './DepositContractsManager';
+import PopulousToken from './PopulousToken';
+
+export default {
+  Populous,
+  Crowdsale,
+  CurrencyToken,
+  DepositContractsManager,
+  PopulousToken,
+};
