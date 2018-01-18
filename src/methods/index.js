66fa901e1a918547cf5f6f270b855cb22a63fb12
kirilltverdev <kirilltverdohlebdev@gmail.com>
2018-01-18 09:39:02 +0200

    Minor fixes

--- src/methods/index.js
+++ src/methods/index.js
@@ -2,13 +2,12 @@ import { connect } from './connect';
 import event from './event';
 import account from './account';
 import transaction from './transaction';
-import {encode, encrypt} from './abi';
+import * as abi from './abi';
 
 export default {
   connect: connect,
   account: account,
   event: event,
   transaction: transaction,
-  encode: encode,
-  encrypt: encrypt
+  abi: abi
 };
