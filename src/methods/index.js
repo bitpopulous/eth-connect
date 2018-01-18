ee6afca8749c89a415228203371d8913591c2200
kirilltverdev <35269043+kirilltverdev@users.noreply.github.com>
2018-01-18 12:05:18 +0200

    #49 Added abi encode functionality (#52)
    * Added abi encode functionality
    
    * Minor fixes
    
    * Minor fixes

--- src/methods/index.js
+++ src/methods/index.js
@@ -2,10 +2,12 @@ import { connect } from './connect';
 import event from './event';
 import account from './account';
 import transaction from './transaction';
+import abi from './abi';
 
 export default {
   connect: connect,
   account: account,
   event: event,
   transaction: transaction,
+  abi: abi,
 };
