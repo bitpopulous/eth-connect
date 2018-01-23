9f9194bd57f56d385d90947846f81f9b8f7bc214
kirilltverdev <kirilltverdohlebdev@gmail.com>
2018-01-23 12:40:40 +0200

    Remove add abi encode functionality

--- src/methods/index.js
+++ src/methods/index.js
@@ -2,12 +2,10 @@ import { connect } from './connect';
 import event from './event';
 import account from './account';
 import transaction from './transaction';
-import abi from './abi';
 
 export default {
   connect: connect,
   account: account,
   event: event,
   transaction: transaction,
-  abi: abi,
 };
